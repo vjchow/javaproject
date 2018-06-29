6557a5ee5fce096346947b223a9777012fdedf4c
vijaybhaskerch <vijaybhaskerch@gmail.com>
2018-06-29 12:31:14 -0500

    This is the shopping cart update

--- Constructors/src/com/walmart/constructor/Main.java
+++ Constructors/src/com/walmart/constructor/Main.java
@@ -4,8 +4,8 @@ public class Main {
 
 	public static void main(String[] args) {
 		
-		
-
+		ShoppingCart shoppingcart = new ShoppingCart(5, 1, 10, "Mens brown Shoes", 25.50 );
+				shoppingcart.shoppingCart(2); 
 	}
 
 }
--- Constructors/src/com/walmart/constructor/ShoppingCart.java
+++ Constructors/src/com/walmart/constructor/ShoppingCart.java
@@ -2,7 +2,71 @@ package com.walmart.constructor;
 
 public class ShoppingCart {
 
-private String addItem;
-private String removeItem;
-
+	private int addItem;
+	private int removeItem;
+	private int quantityOfItem; 
+	private String nameOfItem;
+	private double price;
+	
+	public ShoppingCart() {
+		System.out.println("This is an empty cart");
+	}
+	
+	public ShoppingCart(int addItem, int removeItem, int quantityOfItem, String nameOfItem, double price) {
+		this.addItem = addItem;
+		this.removeItem = removeItem; 
+		this.quantityOfItem = quantityOfItem; 
+		this.nameOfItem = nameOfItem;
+		this.price = price;
+	}
+	
+	
+	
+	public void shoppingCart(int updateShoppingCart) {
+		this.quantityOfItem = addItem + removeItem;
+		updateShoppingCart = quantityOfItem + addItem; 
+		
+		System.out.println("The quantity of items is " + this.quantityOfItem + updateShoppingCart);
+	}
+	
+	
+	public void setAddItem(int addItem) {
+		this.addItem = addItem; 
+	}
+	
+	public int getAddItem() {
+		return addItem; 
+	}
+	
+	public void setRemoveItem(int removeItem) {
+		this.removeItem = removeItem;
+	}
+	
+	public int getRemoveItem() {
+		return removeItem;
+	}
+	
+	public void setQuantityOfItem(int quantityOfItem) {
+		this.quantityOfItem = quantityOfItem;
+	}
+	
+	public int getQuantityOfItem() {
+		return quantityOfItem;
+	}
+	
+	public void setNameOfItem(String nameOfItem) {
+		this.nameOfItem = nameOfItem; 
+	}
+	
+	public String getNameOfItem() {
+		return nameOfItem; 
+	}
+	
+	public void setPrice(double price) {
+		this.price = price;
+	}
+	
+	public double getPrice() {
+		return price; 
+	}
 }
